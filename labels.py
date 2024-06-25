bc83c943442dc2aba3fcbd83987662878abfa3a1
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2024-06-25 12:36:18 -0500

    modified the arguments to run the script from  the terminal

--- labels.py
+++ labels.py
@@ -1,14 +1,10 @@
 import os
-import pandas as pd
+try:
+    import pandas as pd
+except Exception as e:
+    print(f'An error ocurred: {e}.\nTry <pip install pandas> or <pip3 install pandas>')
+import argparse
 
-FILES = [
-    'Dataset/Non_Demented', 
-    'Dataset/Very_Mild_Demented', 
-    'Dataset/Mild_Demented', 
-    'Dataset/Moderate_Demented'
-]
-
-SAVE_PATH = '/Users/inigoparra/Desktop/Run2Recommend/labels.csv'
 
 def get_file_labels(path, label):
     filenames = os.listdir(path)
@@ -27,7 +23,25 @@ def get_items(files):
 
 
 if __name__ == "__main__":
-    
-    flat = get_items(FILES)
-    df = pd.DataFrame(flat, columns=['name', 'label'])
-    df.to_csv(SAVE_PATH)
+
+    parser = argparse.ArgumentParser(description='Create a .csv file for the __getitem__ in the Dataset class.')
+    parser.add_argument('--files_dirs', nargs='+', required=True)
+    parser.add_argument('--out_dir', type=str, required=True)
+    parser.add_argument('--col_names', nargs='+', required=False)
+    args = parser.parse_args()
+
+    files = args.files_dirs
+
+    # FILES = [
+    #     'Dataset/Non_Demented', 
+    #     'Dataset/Very_Mild_Demented', 
+    #     'Dataset/Mild_Demented', 
+    #     'Dataset/Moderate_Demented'
+    # ]
+
+    if args.out_path:
+        assert os.path.isdir(args.out_path), 'Insert a valid directory.'
+
+        flat = get_items(files)
+        df = pd.DataFrame(flat, columns=args.col_names)
+        df.to_csv(args.out_dir)
